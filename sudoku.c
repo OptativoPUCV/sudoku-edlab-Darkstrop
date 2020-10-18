e7b722e04b98be6c51b01a775084c0d55b972c55
repl.it user <replituser@example.com>
2020-10-18 22:15:26 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -122,11 +122,16 @@ int is_final(Node* n)
           }
           n->sudo[f][g]=0;
         }
+        return 1;
+      }
+      else
+      {
+        return 0;
       }
     }
   }
   
-  return 1;
+  return 0;
 }
 
 Node* DFS(Node* initial, int* cont)
