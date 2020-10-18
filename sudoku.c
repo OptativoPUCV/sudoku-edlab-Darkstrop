c41412394116379bfa27c33713f795e8e03bb3f2
repl.it user <replituser@example.com>
2020-10-18 17:46:27 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -3,43 +3,53 @@
 #include "list.h"
 
 
-typedef struct{
-   int sudo[9][9];
+typedef struct
+{
+  int sudo[9][9];
 }Node;
 
-Node* createNode(){
+Node* createNode()
+{
   Node* n=(Node*) malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+Node* copy(Node* n)
+{
+  Node* new=(Node*) malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
-Node* read_file (char* file_name){
+Node* read_file (char* file_name)
+{
   Node* n = createNode();
   FILE* file = fopen (file_name, "r");
   int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          fscanf (file, "%d", &n->sudo[i][j]);
-       }
+  for(i=0;i<9;i++)
+  {
+    for(j=0;j<9;j++)
+    {
+      fscanf (file, "%d", &n->sudo[i][j]);
+    }
   }
 
   fclose (file);
   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
+void print_node(Node* n)
+{
+  int i,j;
+  for(i=0;i<9;i++)
+  {
+    for(j=0;j<9;j++)
+    {
+      printf("%d ", n->sudo[i][j]);
+      printf("\n");
     }
-    printf("\n");
+  }
+  printf("\n");
 }
 
 int is_valid(Node* n)
