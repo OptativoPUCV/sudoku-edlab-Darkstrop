40a019daf252b2b7d6be41a35241bfb6eb58c1c7
repl.it user <replituser@example.com>
2020-10-19 03:46:54 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,8 +50,12 @@ int is_valid(Node* n)
     
   for (i=0; i<9; i++)
   {
-      for (j=0; j<9; j++){ 
-        if (n->sudo[i][j] == 0) continue;
+    for (j=0; j<9; j++)
+    { 
+      if (n->sudo[i][j] == 0)
+      {
+         continue;
+      }
 
         for (k=0; k<9; k++){
 
