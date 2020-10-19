ee49d196be931044412bbe6155cccc9c6e399f93
repl.it user <replituser@example.com>
2020-10-19 02:52:21 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -133,7 +133,10 @@ Node* DFS(Node* initial, int* cont)
   {
     Node* ewe = top(Storm);
     pop(Storm);
-    if(is_final(ewe)==1)return ewe;
+    if(is_final(ewe)==1)
+    {
+      return ewe;
+    }
     List* adj= get_adj_nodes(ewe);
     Node* owo= first(adj);
     while(owo)
@@ -141,11 +144,11 @@ Node* DFS(Node* initial, int* cont)
       push(Storm,owo);
       owo=next(adj);
     }
-  free(ewe);
   }
   return NULL;
 }
- 
+
+
 
 /*
 int main( int argc, char *argv[] ){
