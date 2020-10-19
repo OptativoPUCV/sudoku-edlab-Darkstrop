07dc0063059dc6ab89fa84f68be8a642a0eff02a
repl.it user <replituser@example.com>
2020-10-19 03:49:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,9 +70,9 @@ int is_valid(Node* n)
           }
         }
       }
-        k= i + (j/3);
+        k= 3*(i/3) + (j/3);
         for(int p=0;p<9;p++){
-            int x= k + (p/3) ;
+            int x= 3*(k/3) + (p/3) ;
             int d= 3*(k%3) + (p%3) ;
             if (n->sudo[i][j] == n->sudo[x][d] && (i!=x && j!=d)){
               return 0;
