7e458fc4b02f49dd52c7d784684c061d8470301b
repl.it user <replituser@example.com>
2020-10-19 03:32:37 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -3,174 +3,137 @@
 #include "list.h"
 
 
-typedef struct
-{
-  int sudo[9][9];
+typedef struct{
+   int sudo[9][9];
 }Node;
 
-Node* createNode()
-{
+Node* createNode(){
   Node* n=(Node*) malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n)
-{
-  Node* new=(Node*) malloc(sizeof(Node));
-  *new = *n;
-  return new;
+Node* copy(Node* n){
+    Node* new=(Node*) malloc(sizeof(Node));
+    *new = *n;
+    return new;
 }
 
-Node* read_file (char* file_name)
-{
+Node* read_file (char* file_name){
   Node* n = createNode();
   FILE* file = fopen (file_name, "r");
   int i,j;
-  for(i=0;i<9;i++)
-  {
-    for(j=0;j<9;j++)
-    {
-      fscanf (file, "%d", &n->sudo[i][j]);
-    }
+  for(i=0;i<9;i++){
+       for(j=0;j<9;j++){
+          fscanf (file, "%d", &n->sudo[i][j]);
+       }
   }
 
   fclose (file);
   return n;
 }
 
-void print_node(Node* n)
-{
-  int i,j;
-  for(i=0;i<9;i++)
-  {
-    for(j=0;j<9;j++)
-    {
-      printf("%d ", n->sudo[i][j]);
-      printf("\n");
+void print_node(Node* n){
+    int i,j;
+    for(i=0;i<9;i++){
+       for(j=0;j<9;j++)
+          printf("%d ", n->sudo[i][j]);
+       printf("\n");
     }
-  }
-  printf("\n");
+    printf("\n");
 }
 
-int is_valid(Node* n)
-{/*
-int f;
-int g;
-int h;
+int is_valid(Node* n){
+    int i, j, k;
+    
+    //Linear check
+    for (i=0; i<9; i++){
+      //Create an array, it saves numbers to compare
+      for (j=0; j<9; j++){ 
+        if (n->sudo[i][j] == 0) continue;
 
-for( f=0 ; f<9 ; f++ )
-{
-  for( g=0 ; g<9 ; g++ )
-  {
-    if (n->sudo[f][g] == 0) continue;
-    for( h=0 ; h<9 ; h++ )
-    {
-       if (n->sudo[f][g] == n->sudo[f][h] && g != h)
-        {
-        return 0;
+        for (k=0; k<9; k++){
+
+            if (n->sudo[i][j] == n->sudo[i][k] && j != k){
+              return 0;
+            }
+            if (n->sudo[i][j] == n->sudo[k][j] && i != k){
+              return 0;
+            }
         }
-        else
-        {
-          if (n->sudo[f][g] == n->sudo[h][g] && f != h)
-          {
-            return 0;
-          }
-        }
-    }
-    int ewe=0;
-    int owo=0;
-    int awa=0;
-    h= f+(g/3);
-    while(ewe<9)
-    {
-      owo= h + (ewe/3);
-      awa= 3*(h%3) + (ewe%3);
-      if(n->sudo[f][g]==n->sudo[owo][awa]&&(f!=owo && g!=awa))
-      {
-      	return 0;
+        k= 3*(i/3) + (j/3);
+        for(int p=0;p<9;p++){
+            int x= 3*(k/3) + (p/3) ;
+            int d= 3*(k%3) + (p%3) ;
+            if (n->sudo[i][j] == n->sudo[x][d] && (i!=x && j!=d)){
+              return 0;
+            }
+        }        
       }
-     ewe++;
     }
-  }
-}*/
   return 1;
 }
 
 
-List* get_adj_nodes(Node* n)
-{
-  int f;
-  int g;
-  int numero;
-  List* list=createList();
-  
-  for( f=0 ; f<9 ;f++ )
-  {
-    for( g=0 ; g<9 ; g++ )
+List* get_adj_nodes(Node* n){
+    //List* list=createList();
+    int f;
+    int g;
+    int numero;
+    List* list=createList();
+
+    for( f=0 ; f<9 ;f++ )
     {
-      if(n->sudo[f][g]==0)
+      for( g=0 ; g<9 ; g++ )
       {
-        for( numero=1 ; numero<10 ; numero++)
+        if(n->sudo[f][g]==0)
         {
-          while(numero<10)
+          for( numero=1 ; numero<10 ; numero++)
           {
             n->sudo[f][g]=numero;
             if(is_valid(n))
             {
               Node* nuevo=copy(n);
-              pushBack(list, nuevo);        
-            }
-            numero++;
+              pushBack(list, nuevo);
+            }          
           }
-          n->sudo[f][g]=0;
+        n->sudo[f][g]=0;
+        return list;
         }
       }
     }
-  }
+    
   return list;
 }
 
-
-int is_final(Node* n)
-{
-  int f;
-  int g;
-  for( f=0 ; f<9 ;f++ )
-  {
-    for( g=0 ; g<9 ; g++ )
-    {
-      if(n->sudo[f][g] == 0)
-      {
-        return 0;
-      }
+//Perfect
+int is_final(Node* n){
+    int i,j;
+    for (i=0; i<9; i++){
+        for (j=0; j<9; j++){
+          if (n->sudo[i][j]==0) return 0;
+        }
     }
-  }
-  return 1;
+    return 1;
 }
 
-Node* DFS(Node* initial, int* cont)
-{
-  Stack *Storm = createStack();
-  push(Storm,initial);
-
-  while (get_size(Storm) != 0)
-  {
-    Node* ewe = top(Storm);
-    pop(Storm);
-    if(is_final(ewe)==1)
-    {
-      return ewe;
-    }
-    List* adj= get_adj_nodes(ewe);
-    Node* owo= first(adj);
-    while(owo)
-    {
-      push(Storm,owo);
-      owo=next(adj);
-    }
+Node* DFS(Node* initial, int* cont){
+  Stack *s = createStack();
+  push(s,initial);
+  while (get_size(s) != 0){
+      Node * h= top(s);
+      pop(s); 
+      //n = top(s)
+      if (is_final(h) == 1) return h;
+      List * adj = get_adj_nodes(h); 
+      Node * aux= first(adj);
+      while (aux){
+        push(s,aux);
+        aux= next(adj);
+      }
   }
   return NULL;
-}
+} 
+
 
 
 
